bd5ec04e9c2b02cbe9258595bf744b9a237fec0d
Tolulope Dada <tdada0912@gmail.com>
2022-06-16 02:52:09 +0100

    updated the page to fetch only the first ten posts and implemented a delete function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let postWrapper = document.querySelector("#post-wrapper");
 let postBox = [];
+let firstPostBox = [];
 let createPostForm = document.querySelector("#create-post-form");
 let postTitle = document.querySelector("#post-title");
 let postMessage = document.querySelector("#post-message");
+let deletePost = document.querySelector("#delete-post");
 
 function getPosts () {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -10,9 +12,10 @@ function getPosts () {
         .then((data) => {
             postBox = data;
             // console.log(postBox);
+            firstPostBox = postBox.slice(0, 9);
             let postHandler = '';
 
-            postBox.forEach(post => {
+            firstPostBox.forEach(post => {
                 postHandler += `<div class="col-lg-4">
                                     <div class="card mb-4">
                                         <div class="card-title">
@@ -28,7 +31,9 @@ function getPosts () {
                                             <div class="update-button">
                                                 <button type="submit" class="btn btn-success me-2">Update</button>
                                             </div>
-                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                            <div class="delete-features-button" id="delete-post">
+                                                <button type="submit" onclick="deleteMyPost(${post.id})" class="btn btn-danger">Delete</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>    
@@ -79,7 +84,9 @@ function createPost(event) {
                                             <div class="update-button">
                                                 <button type="submit" class="btn btn-success me-2">Update</button>
                                             </div>
-                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                            <div class="delete-features-button" id="delete-post">
+                                                <button type="submit" onclick="deleteMyPost(${post.id})" class="btn btn-danger">Delete</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>    
@@ -89,3 +96,46 @@ function createPost(event) {
             postWrapper.innerHTML = postHandler;
         })   
 }
+
+function deleteMyPost(id) {
+    fetch('https://jsonplaceholder.typicode.com/posts' + "/" + id, {
+        method: 'DELETE',
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        postBox.shift(data);
+        console.log(postBox);
+
+        let postHandler = '';
+        postBox.forEach(post => {
+            postHandler += `<div class="col-lg-4">
+                                    <div class="card mb-4">
+                                        <div class="card-title">
+                                            <div class="index">
+                                                <p>${post.id}</p>
+                                            </div>
+                                            <p>${post.title}</p>
+                                        </div>
+                                        <div class="card-body px-0">
+                                            <p>${post.body}</p>
+                                        </div>
+                                        <div class="card-buttons mt-3 d-flex justify-content-end">
+                                            <div class="update-button">
+                                                <button type="submit" class="btn btn-success me-2">Update</button>
+                                            </div>
+                                            <div class="delete-features-button" id="delete-post">
+                                                <button type="submit" onclick="deleteMyPost(${post.id})" class="btn btn-danger">Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>    
+                                        `
+        })
+        postWrapper.innerHTML = postHandler;
+    })
+
+}
+
