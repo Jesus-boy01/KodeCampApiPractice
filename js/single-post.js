df36ccd620c9587eb10cd7f004f7cac0e95d3725
Tolulope Dada <tdada0912@gmail.com>
2022-06-17 14:29:13 +0100

    code optimization

--- js/single-post.js
+++ js/single-post.js
@@ -1,7 +1,6 @@
 function renderSelectedPost() {
     let viewPostObject = localStorage.getItem('viewedPost');
     let viewMyPost = JSON.parse(viewPostObject);
-    // console.log(viewMyPost.body);
     document.querySelector("#view-post-id").innerHTML = viewMyPost.id;
     document.querySelector("#view-post-title").innerHTML = viewMyPost.title;
     document.querySelector("#view-post-body").innerHTML = viewMyPost.body;
